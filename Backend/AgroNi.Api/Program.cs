e5860df811a4f94831d78bb3a19d8d1ca6ee64fd
saidmarincastillo-ux <saidmarincastillo@gmail.com>
2026-08-12 22:15:18 -0600

    archivos faltante

---
+++ Backend/AgroNi.Api/Program.cs
@@ -0,0 +1,37 @@
+using AgroNi.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=app.db"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
+app.Urls.Add("http://0.0.0.0:5000");
+app.Run();
---
+++ Frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.agroni.app',
+  appName: 'AgroNi',
+  webDir: 'dist'
+};
+
+export default config;
